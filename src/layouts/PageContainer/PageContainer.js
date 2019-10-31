5b5ef1c6e790d54272b5b54f8e31e440d4a34bd8
blawabar <fake.torrent@wp.pl>
2019-10-31 23:36:23 +0100

    add small styling for PageContainer

--- src/layouts/PageContainer/PageContainer.js
+++ src/layouts/PageContainer/PageContainer.js
@@ -1,18 +1,19 @@
 import React from "react";
-
 import { Switch, Route } from "react-router-dom";
+
+import "./PageContainer.scss";
 
 import Explorer from "../../pages/Explorer/Explorer";
 import PostContent from "../../pages/PostContent/PostContent";
-
+// it must have at least height of 100vh - (Header + Footer)
 const PageContainer = () => {
   return (
-    <>
+    <div className="page-container">
       <Switch>
         <Route exact path="/" component={Explorer} />
         <Route exact path="/:postId" component={PostContent} />
       </Switch>
-    </>
+    </div>
   );
 };
 

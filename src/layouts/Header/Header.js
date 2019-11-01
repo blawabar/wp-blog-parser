617d9a14aaf3fe69dba140686aa9c66db918b1bc
blawabar <fake.torrent@wp.pl>
2019-11-01 18:13:37 +0100

    add Header content

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import "./Header.scss";
 
+import logo from "../../img/logo.png";
+
 const Header = () => {
   return (
     <header className="main-header">
+      <img src={logo} alt="" className="main-header__logo" />
       <h1 className="main-header__title">Explore the world of Wordpress</h1>
       <h3 className="main-header__sub-title">
         search through plethora of posts right now

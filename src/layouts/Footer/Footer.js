1d851cb33ffabae5e53d8a40bd1bc2915283ba5c
blawabar <fake.torrent@wp.pl>
2019-11-02 00:49:31 +0100

    add small changes in index, Footer, Header and  PageContainer

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -5,7 +5,7 @@ import "./Footer.scss";
 export default () => {
   return (
     <footer className="main-footer">
-      <p className="main-footer__info">Bartosz Krolak &copy;2019 </p>
+      <p className="main-footer__info">&copy;&nbsp;Bartosz Krolak 2019 </p>
     </footer>
   );
 };

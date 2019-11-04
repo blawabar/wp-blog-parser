d92e7f56afa57ca61e287cdae680fbacfe1e73b1
blawabar <fake.torrent@wp.pl>
2019-11-04 22:17:10 +0100

    add search engine for Explorer

--- src/pages/Explorer/Explorer.js
+++ src/pages/Explorer/Explorer.js
@@ -2,19 +2,134 @@ import React, { useState } from "react";
 
 import "./Explorer.scss";
 
+// Sekcje Results and Form zostaną rozbite na komponenty
+
 const Explorer = () => {
-  const [searchState, setSearchState] = useState({
-    url: "",
+  const [searchData, setsearchData] = useState({
+    domain: "",
     searchPhrase: "",
     searchLimit: 5,
     orderBy: "date"
   });
 
-  const handleOnSubmit = evt => evt.preventDefault();
+  const [fetchResult, setFetchResult] = useState({
+    isLoading: false,
+    data: null,
+    error: null
+  });
+
+  const createBaseURL = blogDomain =>
+    `https://public-api.wordpress.com/rest/v1.1/sites/${blogDomain}/posts/?fields=ID,author,date,modified,title,short_URL,excerpt,attachments`;
+
+  const createQueryParams = (searchPhrase, searchLimit, orderBy) => {
+    const queryParams = new URLSearchParams({
+      number: searchLimit,
+      order_by: orderBy
+    });
+
+    if (searchPhrase) {
+      queryParams.set("search", searchPhrase);
+    }
+
+    return queryParams.toString();
+  };
+
+  const sendRequest = async (url, queryParams) => {
+    try {
+      setFetchResult({ isLoading: true, data: null, error: null });
+
+      const response = await fetch(`${url}&${queryParams}`);
+
+      if (response.ok) {
+        const data = await response.json();
+        setFetchResult({ ...fetchResult, isLoading: false, data });
+        // console.log({ data });
+      } else {
+        const error = await response.json();
+        // console.error({ error });
+        setFetchResult({ ...fetchResult, isLoading: false, error });
+      }
+    } catch (error) {
+      console.error({ error });
+    }
+  };
+
+  const isDomainNameValid = domainName => {
+    const regExp = /^[a-z0-9][a-z0-9-]*[a-z0-9](\.[a-z0-9]+[a-z0-9-]*[a-z0-9]+)*\.[a-z][a-z]*[a-z]$/;
+    regExp.test(domainName);
+
+    return (
+      regExp.test(domainName) &&
+      domainName.length >= 4 &&
+      domainName.length <= 100
+    );
+  };
+
+  const isSearchLimitValid = searchLimit =>
+    searchLimit >= 5 && searchLimit <= 100;
+
+  const handleOnSubmit = evt => {
+    evt.preventDefault();
+
+    // 0. Get form data
+    const { domain, searchPhrase, searchLimit, orderBy } = searchData;
+    // console.log({ domain, searchPhrase, searchLimit, orderBy });
+
+    if (!isDomainNameValid(domain)) {
+      alert("Please enter a valid domain name - max. 100 chars");
+      return;
+    }
+
+    // 2. Validate searchLimit for the range 5 - 100,
+    if (!isSearchLimitValid(searchLimit)) {
+      alert("Please provide a valid search value from 5 to 100");
+      return;
+    }
+
+    // 3. Send request - it should be moved into the sendRequest method
+    // - prepare base url,
+    const baseURL = createBaseURL(domain);
+    // - create queryParams
+    const queryParams = createQueryParams(searchPhrase, searchLimit, orderBy);
+
+    sendRequest(baseURL, queryParams); // it should accept searchData as the parameter
+  };
 
   const handleOnChange = evt => {
     const { name, value } = evt.target;
-    setSearchState({ ...searchState, [name]: value });
+    setsearchData({ ...searchData, [name]: value });
+  };
+
+  const renderPostItems = fetchData => {
+    const { isLoading, data, error } = fetchData;
+
+    if (isLoading) {
+      return <h1>Loading data...</h1>;
+    } else {
+      if (error) {
+        return <h1>An Error has occured: {error}</h1>;
+      }
+      if (data) {
+        return (
+          <>
+            <h2>Found {data.posts.length} posts:</h2>
+            {data.posts.map(
+              ({ ID, author, date, modified, title, short_URL, excerpt }) => (
+                <div key={ID}>
+                  <div>{ID}</div>
+                  <p>{author.name}</p>
+                  <p>{date}</p>
+                  <p>{modified}</p>
+                  <p>{title}</p>
+                  <p>{short_URL}</p>
+                  <p>{excerpt}</p>
+                </div>
+              )
+            )}
+          </>
+        );
+      }
+    }
   };
 
   return (
@@ -23,10 +138,10 @@ const Explorer = () => {
         <form className="search-form" noValidate onSubmit={handleOnSubmit}>
           <input
             type="text"
-            name="url"
+            name="domain"
             className="search-form__input-text"
-            placeholder="Wordpress URL"
-            value={searchState.url}
+            placeholder="Wordpress domain"
+            value={searchData.url}
             onChange={handleOnChange}
           />
           <input
@@ -34,7 +149,7 @@ const Explorer = () => {
             name="searchPhrase"
             className="search-form__input-text"
             placeholder="Search phrase"
-            value={searchState.searchPhrase}
+            value={searchData.searchPhrase}
             onChange={handleOnChange}
           />
           <input
@@ -44,7 +159,7 @@ const Explorer = () => {
             placeholder="Limit"
             min="5"
             max="100"
-            value={searchState.searchLimit}
+            value={searchData.searchLimit}
             onChange={handleOnChange}
           />
           <div className="search-form__input-holder">
@@ -52,7 +167,7 @@ const Explorer = () => {
             <select
               name="orderBy"
               className="search-form__input-select"
-              value={searchState.orderBy}
+              value={searchData.orderBy}
               onChange={handleOnChange}
             >
               <option value="date">Date</option>
@@ -67,6 +182,7 @@ const Explorer = () => {
       </section>
       <section className="explorer__results">
         <h1>Results</h1>
+        {renderPostItems(fetchResult)}
       </section>
     </div>
   );

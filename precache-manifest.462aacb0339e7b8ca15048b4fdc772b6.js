4699c455cbabf34eff94a81d6507d02362c8d2ff
blawabar <fake.torrent@wp.pl>
2020-02-12 20:53:06 +0100

    Updates

---
+++ precache-manifest.462aacb0339e7b8ca15048b4fdc772b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236074e3884f0af55714321352a2662d",
+    "url": "/wp-blog-parser/index.html"
+  },
+  {
+    "revision": "1d83c1bacd4b640e72d1",
+    "url": "/wp-blog-parser/static/css/main.48d7da5a.chunk.css"
+  },
+  {
+    "revision": "e4492c0df82dc61be7bc",
+    "url": "/wp-blog-parser/static/js/2.e9f139e6.chunk.js"
+  },
+  {
+    "revision": "1d83c1bacd4b640e72d1",
+    "url": "/wp-blog-parser/static/js/main.21eb3392.chunk.js"
+  },
+  {
+    "revision": "217ce1bde61b15d8f7cd",
+    "url": "/wp-blog-parser/static/js/runtime-main.e87f967e.js"
+  },
+  {
+    "revision": "f00e7a8abfba3abdf7299154b34b1e26",
+    "url": "/wp-blog-parser/static/media/alphabet.f00e7a8a.jpg"
+  },
+  {
+    "revision": "21fb02aea4a79288b1666698b49b812b",
+    "url": "/wp-blog-parser/static/media/logo.21fb02ae.png"
+  }
+]);
